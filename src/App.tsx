88f2f2df06c8f679d09ce28698e81d1faf87ff7f
ygt-design <103063122+ygt-design@users.noreply.github.com>
2025-04-09 10:14:34 -0400

    deploy commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import Home from './pages/Home/Home';
 import WhoWeAre from './pages/WhoWeAre/WhoWeAre';
 import CaseStudies from './pages/CaseStudies/CaseStudies';
 import GetStarted from './pages/GetStarted/GetStarted';
-import ProductTestPage from './pages/ProductTestPage/ProductTestPage'; // Import Mock Page
-import CaseStudyPage from './pages/CaseStudyPages/CaseStudyPage'; // Import Mock Page
+import ProductTestPage from './pages/ProductTestPage/ProductTestPage'; // Mock Page for testing
+import CaseStudyPage from './pages/CaseStudyPages/CaseStudyPage'; // Mock Page for testing
 import Product from './pages/Products/Products';
 import ProductPage from './pages/ProductPages/ProductPage';
 import { LikedProductsProvider } from './context/LikedProductsContext';
@@ -14,21 +14,21 @@ import { LikedProductsProvider } from './context/LikedProductsContext';
 const App = () => {
   return (
     <LikedProductsProvider>
-    <Router>
-      <ScrollToTop />
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/who-we-are" element={<WhoWeAre />} />
-          <Route path="/products" element={<Product />} />
-          <Route path="/case-studies" element={<CaseStudies />} />
-          <Route path="/case-study/:id" element={<CaseStudyPage />} /> {/* 🔹 New Route */}
-          <Route path="/products/:id" element={<ProductPage />} />
-          <Route path="/get-started" element={<GetStarted />} />
-          <Route path="/product-test" element={<ProductTestPage />} /> 
-        </Routes>
-      </Layout>
-    </Router>
+      <Router>
+        <ScrollToTop />
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/who-we-are" element={<WhoWeAre />} />
+            <Route path="/products" element={<Product />} />
+            <Route path="/case-studies" element={<CaseStudies />} />
+            <Route path="/case-study/:id" element={<CaseStudyPage />} />
+            <Route path="/products/:id" element={<ProductPage />} />
+            <Route path="/get-started" element={<GetStarted />} />
+            <Route path="/product-test" element={<ProductTestPage />} /> 
+          </Routes>
+        </Layout>
+      </Router>
     </LikedProductsProvider>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/projectory/', // Ensures asset paths are built correctly
   plugins: [react()],
 })
