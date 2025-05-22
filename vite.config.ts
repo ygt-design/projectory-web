8967710fdd1be3a3f21ba98b2667abcafcf46e12
ygt-design <103063122+ygt-design@users.noreply.github.com>
2025-05-22 11:21:29 -0400

    cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "./",
+  base: '/',            // root‐relative asset URLs
   plugins: [react()],
   build: {
-    outDir: "docs",
+    outDir: 'dist',
     emptyOutDir: true
   }
 })
