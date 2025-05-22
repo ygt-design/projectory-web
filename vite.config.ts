a0258fe2d4b66a74412aacd2548d9b4cb97c9de1
ygt-design <103063122+ygt-design@users.noreply.github.com>
2025-05-22 10:06:57 -0400

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // Updated to root for correct routing
+  base: '/', // ensures all asset URLs are root-relative
   plugins: [react()],
 })
